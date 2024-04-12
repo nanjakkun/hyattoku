ffba504f8f95722379ef02f085c67d6df8ed4b18
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-13 04:00:16 +0900

    Install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // https://astro.build/config
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://nanjakkun.github.io',
   base: '/juttoku',
-})
+  integrations: [react()]
+});

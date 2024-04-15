88d5703ff6fddfb6b0d91a275ef87636dbd8f832
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-16 05:26:12 +0900

    Enable to output female names

--- src/components/NamingJa.tsx
+++ src/components/NamingJa.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import { useEffect, useState } from "react";
-import {
-  FamilyNamesJa,
-  FirstNamesMalesJa,
-} from "./naming/names_ja.ts";
+import { FamilyNamesJa } from "./naming/FamilyNamesJa.ts";
+import { FirstNamesMalesJa } from "./naming/FirstNamesMalesJa.ts";
+import { FirstNamesFemalesJa } from "./naming/FirstNamesFemalesJa.ts";
 
 import { Button } from "./common/Button.tsx";
 
@@ -25,9 +25,20 @@ export const NamingJa = () => {
     const _names: string[] = new Array(10).fill(undefined).map(() => {
       const idx1 = Math.floor(Math.random() * FamilyNamesJa.length);
       const family_name = FamilyNamesJa[idx1];
+      let first_name = undefined;
 
-      const idx2 = Math.floor(Math.random() * FirstNamesMalesJa.length);
-      const first_name = FirstNamesMalesJa[idx2];
+      const rand: number = Math.floor(Math.random() * 2);
+
+      switch (rand) {
+        case 0:
+          const idx2 = Math.floor(Math.random() * FirstNamesMalesJa.length);
+          first_name = FirstNamesMalesJa[idx2];
+          break;
+        default:
+          const idx3 = Math.floor(Math.random() * FirstNamesFemalesJa.length);
+          first_name = FirstNamesFemalesJa[idx3];
+          break;
+      }
 
       return family_name.name + "　" + first_name.name;
     });
@@ -44,7 +55,7 @@ export const NamingJa = () => {
       </p>
       <div className="flex justify-center">
         <textarea
-          className="w-4/5 h-52 border border-black solid text-black bg-gray-300"
+          className="w-4/5 h-72 border border-black solid text-black bg-gray-300"
           value={text}
           readOnly
         />

e3a1c9d1cdefad582de80cf40249804cc47c6e22
KIMURA Takahiro <nanjakkun@gmail.com>
2024-04-18 13:32:09 +0900

    Add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nanjakkun.github.io',
   base: '/hyattoku',
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
       noExternal: ["qrcode.react"],

3fd56f1064eb5a673690e03ed439e7217b8b5465
KIMURA Takahiro <nanjakkun@gmail.com>
2025-12-25 16:24:59 +0900

    Fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,22 @@
-/// <reference types="vitest" />
+import { defineConfig, mergeConfig } from 'vitest/config';
 import { getViteConfig } from 'astro/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default getViteConfig({
-	test: {
-		/* for example, use global to avoid globals imports (describe, test, expect): */
-		// globals: true,
-    include: ["test/*"]
-	},
+export default defineConfig(async (env) => {
+	// getViteConfigは設定を返す
+	const astroViteConfigFn = getViteConfig({
+		plugins: [tsconfigPaths()],
+	});
+
+	// Vitestから受け取ったenvを使う
+	const astroViteConfig = await astroViteConfigFn(env);
+
+	// Vitest固有の設定を定義
+	return mergeConfig(astroViteConfig, {
+		test: {
+			setupFiles: ['src/__tests__/setup.ts'],
+			include: ['src/**/*.{test,spec}.ts'],
+			environment: 'happy-dom',
+		},
+	});
 });
